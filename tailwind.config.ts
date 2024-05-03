add6e27ae1cde6d005271dcfcec08a6e64b170a9
Paul <paul@theapplegates.com>
2024-05-03 14:48:49 -0400

    test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,8 +32,8 @@ export default {
 			},
 			fontFamily: {
 				// Add any custom fonts here
-				sans: [...fontFamily.sans],
-				serif: [...fontFamily.serif],
+				sans: ['Roboto Flex'],
+				serif: ['Roboto Flex'],
 			},
 			transitionProperty: {
 				height: "height",
